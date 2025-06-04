dc9a9fa9fd5d9038e4abc857915be57196c89505
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-04 20:32:10 +0500

    update places_near_you.dart

--- lib/widgets/places_near_you.dart
+++ lib/widgets/places_near_you.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class PlacesNearYou extends StatelessWidget {
   const PlacesNearYou({super.key});
@@ -24,6 +25,59 @@ class PlacesNearYou extends StatelessWidget {
               child: InkWell(
                 onTap: () {},
                 borderRadius: BorderRadius.circular(14),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          12,
+                        ),
+                        child: Image.asset(
+                          "assets/places/place3.jpg",
+                          fit: BoxFit.cover,
+                          width: 130,
+                          height: double.maxFinite,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Sea of Peace",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          const Text(
+                            "Some city name",
+                            textAlign: TextAlign.start,
+                          ),
+                          Spacer(),
+                          Row(
+                            children: [
+                              Icon(
+                                size: 16,
+                                Ionicons.map_outline,
+                              ),
+                              SizedBox(
+                                width: 08,
+                              ),
+                              Text(
+                                "Accra - - - - - ",
+                                style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                ),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
