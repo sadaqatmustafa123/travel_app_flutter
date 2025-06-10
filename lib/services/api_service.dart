9a7b7dbb21be739b1554a5607268f8135a3e8f7b
Sadaqat Mustafa <sadaqatmusatafa12345@gmail.com>
2025-06-10 14:46:03 +0500

    changes in api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,24 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:travel_app/data/users/models/user_model.dart';
 
 class ApiService {
   final String userUrl = "https://api.escuelajs.co/api/v1/users/1";
 
-  Future<dynamic> getSingleUser() async {
-    var response = await http.get(
-      Uri.parse(userUrl),
-    );
+  Future<UserModel?> getSingleUser() async {
+    try {
+      var response = await http.get(Uri.parse(userUrl));
+
+      if (response.statusCode == 200) {
+        var jsonData = json.decode(response.body);
+        return UserModel.fromJson(jsonData);
+      } else {
+        print("Failed to load user. Status code: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      print("Error fetching user: $e");
+      return null;
+    }
   }
 }
